cce3aa7957fad923573c95d575ce65cf02b1aaea
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-09-11 17:37:23 +0200

    Need fix

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('JS OK')
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        emails: [],
+        apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
+        totEmails: 10,
+      }
+    },
+    methods: {
+        getRandomEmail(){
+            for(let i = 0 ; i < totEmails; i++ ){
+                axios.get(this.apiUrl)
+                .then( (response) => {
+                    console.log(response.data.response)
+                    this.emails.push(response.data.response)
+                });
+            }
+            
+        },
+        mounted(){
+            this.getRandomEmail
+        }
+    }
+  }).mount('#app')
